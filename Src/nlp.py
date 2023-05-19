79548541098e536aaa257e3b785a513cd90b52c6
michelestaffieri <michelestaf@gmail.com>
2023-05-19 12:24:33 +0200

    nlp class

--- Src/nlp.py
+++ Src/nlp.py
@@ -1,5 +1,4 @@
 import os
-
 import nltk
 from tqdm import tqdm
 import re
@@ -9,11 +8,8 @@ from Utils.emoji import emojiNeg
 from Utils.emoji import othersEmoji
 from Utils.emoji import negemoticons
 from Utils.emoji import posemoticons
-
 from Utils.slang import slang_words
-
 from Utils.punctuation import punctuation
-
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet
@@ -25,8 +21,8 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 
-res_base_path = "../Resources/Risorse lessicali/Archive_risorse_lessicali/"
-tweets_path = "../Resources/Twitter messaggi/"
+RES_PATH = "../Resources/Risorse lessicali/Archive_risorse_lessicali/"
+TWEETS_PATH = "../Resources/Twitter messaggi/"
 feeling_list = ['Anger', 'Anticipation', 'Disgust', 'Fear', 'Joy', 'Sadness', 'Surprise', 'Trust']
 
 
@@ -41,17 +37,13 @@ class NLPAnalyzer:
         self.analyze_resources()
 
     def pos_tagging(self, word_tokens):
-        # remove stop words
         sw = set(stopwords.words('english'))
-        # remove stop words from word_tokens
         word_tokens = [w for w in word_tokens if (w not in sw)]
 
         tag_dict = {"J": wordnet.ADJ,
                     "N": wordnet.NOUN,
                     "V": wordnet.VERB,
                     "R": wordnet.ADV}
-
-        # Map POS tag to first character lemmatize() accepts
         res = []
         ts = nltk.pos_tag(word_tokens)
         for t in ts:
@@ -67,13 +59,11 @@ class NLPAnalyzer:
 
         for feeling in tqdm(feeling_list):
             self.words[feeling] = []
-            with open(tweets_path + "dataset_dt_" + feeling.lower() + "_60k.txt", 'r', encoding="utf8") as file:
+            with open(TWEETS_PATH + "dataset_dt_" + feeling.lower() + "_60k.txt", 'r', encoding="utf8") as file:
                 lines = file.readlines()
                 print("Start Analyzing tweet. Feeling: ", feeling)
                 for line in lines:
-
                     line = line.replace('USERNAME', '').replace('URL', '')
-
                     if '#' in line:
                         hashtags = re.findall(r"#(\w+)", line)
                         for htag in hashtags:
@@ -97,35 +87,32 @@ class NLPAnalyzer:
                     line = line.lower()
 
                     word_tokens = tk.tokenize(line)
-                    # replace slang from sentences
                     slang_list = [s for s in slang_words.keys() if (s in line.split())]
                     for s in slang_list:
                         line = line.replace(s, slang_words[s])
 
                     pos_line = self.pos_tagging(word_tokens)
 
-                    # lemmatize nouns, adjective, verbs
                     for pos in pos_line:
                         if pos[1] in ['j', 'n', 'v']:
                             lemm_w = lemmatizer.lemmatize(pos[0], pos[1])
                             self.words[feeling].append(lemm_w)
                             lemmatized_tweets[lemm_w] = lemmatized_tweets.get(lemm_w, 0) + 1
-            # Store emoji, tags and tweets for feeling
             self.emoji[feeling] = emoji_list
             self.tweets[feeling] = lemmatized_tweets
             self.tags[feeling] = tag_list
 
     def analyze_resources(self):
         for feeling in tqdm(feeling_list):
-            for feeling_resource in os.listdir(res_base_path + feeling):
+            for feeling_resource in os.listdir(RES_PATH + feeling):
                 if feeling_resource.endswith(".txt"):
-                    with open(res_base_path + feeling + "/" + feeling_resource, 'r', encoding="utf8") as file:
+                    with open(RES_PATH + feeling + "/" + feeling_resource, 'r', encoding="utf8") as file:
                         lines = file.readlines()
                         for line in lines:
-                            if not '_' in line:
+                            if '_' not in line:
                                 line = line.replace('\n', '')
                                 self.resources[feeling, feeling_resource] = self.resources.get(
-                                    (feeling, feeling_resource),[]) + [line]
+                                    (feeling, feeling_resource), []) + [line]
 
 
 if __name__ == '__main__':
