a54738b998692cdef4bac15f44906a2485ca6567
michelestaffieri <michelestaf@gmail.com>
2023-05-19 12:19:52 +0200

    nlp class

--- Src/nlp.py
+++ Src/nlp.py
@@ -25,115 +25,110 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 
-words = {}
-emoji = {}
-tags = {}
-tweets = {}
-resources = {}
-
 res_base_path = "../Resources/Risorse lessicali/Archive_risorse_lessicali/"
 tweets_path = "../Resources/Twitter messaggi/"
 feeling_list = ['Anger', 'Anticipation', 'Disgust', 'Fear', 'Joy', 'Sadness', 'Surprise', 'Trust']
 
 
-def pos_tagging(word_tokens):
-    # remove stop words
-    sw = set(stopwords.words('english'))
-    # remove stop words from word_tokens
-    word_tokens = [w for w in word_tokens if (w not in sw)]
+class NLPAnalyzer:
+    def __init__(self):
+        self.words = {}
+        self.emoji = {}
+        self.tags = {}
+        self.tweets = {}
+        self.resources = {}
+        self.analyze_tweets()
+        self.analyze_resources()
 
-    tag_dict = {"J": wordnet.ADJ,
-                "N": wordnet.NOUN,
-                "V": wordnet.VERB,
-                "R": wordnet.ADV}
+    def pos_tagging(self, word_tokens):
+        # remove stop words
+        sw = set(stopwords.words('english'))
+        # remove stop words from word_tokens
+        word_tokens = [w for w in word_tokens if (w not in sw)]
 
-    # Map POS tag to first character lemmatize() accepts
-    res = []
-    ts = nltk.pos_tag(word_tokens)
-    for t in ts:
-        res.append((t[0], tag_dict.get(str(t[1][0]).upper(), 'q')))
-    return res
+        tag_dict = {"J": wordnet.ADJ,
+                    "N": wordnet.NOUN,
+                    "V": wordnet.VERB,
+                    "R": wordnet.ADV}
 
+        # Map POS tag to first character lemmatize() accepts
+        res = []
+        ts = nltk.pos_tag(word_tokens)
+        for t in ts:
+            res.append((t[0], tag_dict.get(str(t[1][0]).upper(), 'q')))
+        return res
 
-def analyze_tweets():
-    tag_list = {}
-    emoji_list = {}
-    lemmatized_tweets = {}
-    tk = TweetTokenizer()
-    lemmatizer = WordNetLemmatizer()
+    def analyze_tweets(self):
+        tag_list = {}
+        emoji_list = {}
+        lemmatized_tweets = {}
+        tk = TweetTokenizer()
+        lemmatizer = WordNetLemmatizer()
 
-    for feeling in tqdm(feeling_list):
-        words[feeling] = []
-        with open(tweets_path + "dataset_dt_" + feeling.lower() + "_60k.txt", 'r', encoding="utf8") as file:
-            lines = file.readlines()
-            print("Start Analyzing tweet. Feeling: ", feeling)
-            for line in lines:
+        for feeling in tqdm(feeling_list):
+            self.words[feeling] = []
+            with open(tweets_path + "dataset_dt_" + feeling.lower() + "_60k.txt", 'r', encoding="utf8") as file:
+                lines = file.readlines()
+                print("Start Analyzing tweet. Feeling: ", feeling)
+                for line in lines:
 
-                # remove USERNAME and URL
-                line = line.replace('USERNAME', '').replace('URL', '')
+                    line = line.replace('USERNAME', '').replace('URL', '')
 
-                # build map for hashtag and remove from line
-                # li sostituiamo e li teniamo da parte per il conteggio finale
-                if '#' in line:
-                    hashtags = re.findall(r"#(\w+)", line)
-                    for htag in hashtags:
-                        tag_list[htag] = tag_list.get(htag, 0) + 1
-                        line = line.replace('#' + htag, '').replace('#', '')
-                        words[feeling].append(htag)
+                    if '#' in line:
+                        hashtags = re.findall(r"#(\w+)", line)
+                        for htag in hashtags:
+                            tag_list[htag] = tag_list.get(htag, 0) + 1
+                            line = line.replace('#' + htag, '').replace('#', '')
+                            self.words[feeling].append(htag)
 
-                # find, store and replace emoji from line
-                # identificazione e rimozione da ogni tweet delle varie emoji ed emoticon.
-                # Usiamo la libreria Demoji per la codifica
-                ejs = [demoji.replace_with_desc(em, ":") for em in emojiNeg + emojiPos + othersEmoji + negemoticons +
-                       posemoticons if (em in line)]
+                    ejs = [demoji.replace_with_desc(em, ":") for em in
+                           emojiNeg + emojiPos + othersEmoji + negemoticons +
+                           posemoticons if (em in line)]
 
-                for e in ejs:
-                    emoji_list[e] = emoji_list.get(e, 0) + 1
-                    line = line.replace(e, '')
-                    words[feeling].append(e)
+                    for e in ejs:
+                        emoji_list[e] = emoji_list.get(e, 0) + 1
+                        line = line.replace(e, '')
+                        self.words[feeling].append(e)
 
-                # remove punctuation
-                punct_list = [p for p in punctuation if (p in line)]
-                for p in punct_list:
-                    line = line.replace(p, ' ')
+                    punct_list = [p for p in punctuation if (p in line)]
+                    for p in punct_list:
+                        line = line.replace(p, ' ')
 
-                line = line.lower()
+                    line = line.lower()
 
-                # tokenize sentence
-                word_tokens = tk.tokenize(line)
-                # replace slang from sentences
-                slang_list = [s for s in slang_words.keys() if (s in line.split())]
-                for s in slang_list:
-                    line = line.replace(s, slang_words[s])
+                    word_tokens = tk.tokenize(line)
+                    # replace slang from sentences
+                    slang_list = [s for s in slang_words.keys() if (s in line.split())]
+                    for s in slang_list:
+                        line = line.replace(s, slang_words[s])
 
-                pos_line = pos_tagging(word_tokens)
+                    pos_line = self.pos_tagging(word_tokens)
 
-                # lemmatize nouns, adjective, verbs
-                for pos in pos_line:
-                    if pos[1] in ['j', 'n', 'v']:
-                        lemm_w = lemmatizer.lemmatize(pos[0], pos[1])
-                        words[feeling].append(lemm_w)
-                        lemmatized_tweets[lemm_w] = lemmatized_tweets.get(lemm_w, 0) + 1
-        # Store emoji, tags and tweets for feeling
-        emoji[feeling] = emoji_list
-        tweets[feeling] = lemmatized_tweets
-        tags[feeling] = tag_list
+                    # lemmatize nouns, adjective, verbs
+                    for pos in pos_line:
+                        if pos[1] in ['j', 'n', 'v']:
+                            lemm_w = lemmatizer.lemmatize(pos[0], pos[1])
+                            self.words[feeling].append(lemm_w)
+                            lemmatized_tweets[lemm_w] = lemmatized_tweets.get(lemm_w, 0) + 1
+            # Store emoji, tags and tweets for feeling
+            self.emoji[feeling] = emoji_list
+            self.tweets[feeling] = lemmatized_tweets
+            self.tags[feeling] = tag_list
 
-
-def analyze_resources():
-    for feeling in tqdm(feeling_list):
-        for feeling_resource in os.listdir(res_base_path + feeling):
-            if feeling_resource.endswith(".txt"):
-                with open(res_base_path + feeling + "/" + feeling_resource, 'r', encoding="utf8") as file:
-                    lines = file.readlines()
-                    for line in lines:
-                        if not '_' in line:
-                            line = line.replace('\n', '')
-                            resources[feeling, feeling_resource] = resources.get((feeling, feeling_resource), []) \
-                                                                   + [line]
+    def analyze_resources(self):
+        for feeling in tqdm(feeling_list):
+            for feeling_resource in os.listdir(res_base_path + feeling):
+                if feeling_resource.endswith(".txt"):
+                    with open(res_base_path + feeling + "/" + feeling_resource, 'r', encoding="utf8") as file:
+                        lines = file.readlines()
+                        for line in lines:
+                            if not '_' in line:
+                                line = line.replace('\n', '')
+                                self.resources[feeling, feeling_resource] = self.resources.get(
+                                    (feeling, feeling_resource),[]) + [line]
 
 
 if __name__ == '__main__':
-    db = 'MongoDB'  # postgreSQL or MongoDB
-    analyze_resources()
-    analyze_tweets()
+    nlp = NLPAnalyzer()
+    nlp.analyze_tweets()
+    nlp.analyze_resources()
